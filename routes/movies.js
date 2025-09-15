ba132045b7227d8209421ca5c00ba3c181fdb063
Alejandro Arco <alejandroarpu@gmail.com>
2025-09-15 19:31:53 +0200

    feat: add all the controllers, models and routes

--- routes/movies.js
+++ routes/movies.js
@@ -1,77 +1,12 @@
 import { Router } from "express";
-import movies from "../movies.json" with { type: "json" };
-import { validateMovie, validatePartialMovie } from "../schemas/movies.js";
+import { MovieController } from "../controllers/movies.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  const { genre } = req.query;
-  if (genre) {
-    const filteredMovies = movies.filter((movie) =>
-      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
-    );
-    return res.json(filteredMovies);
-  }
-  res.json(movies);
-});
-
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  const movie = movies.find((movie) => movie.id === id);
-  if (!movie) {
-    res.status(404).json({ message: "Movie not found" });
-    return;
-  }
-  res.json(movie);
-});
-
-router.post("/", (req, res) => {
-  const result = validateMovie(req.body);
-
-  if (result.error) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) });
-  }
-
-  const newMovie = {
-    id: crypto.randomUUID(),
-    ...result.data,
-  };
-
-  movies.push(newMovie);
-  res.status(201).json(newMovie);
-});
-
-router.patch("/:id", (req, res) => {
-  const result = validatePartialMovie(req.body);
-
-  if (result.error) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) });
-  }
-
-  const { id } = req.params;
-  const movieIndex = movies.findIndex((movie) => movie.id === id);
-
-  if (movieIndex === -1) {
-    return res.status(404).json({ message: "Movie not found" });
-  }
-
-  const updatedMovie = { ...movies[movieIndex], ...result.data };
-  movies[movieIndex] = updatedMovie;
-
-  return res.json(updatedMovie);
-});
-
-router.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  const movieIndex = movies.findIndex((movie) => movie.id === id);
-
-  if (movieIndex === -1) {
-    return res.status(404).json({ message: "Movie not found" });
-  }
-
-  movies.splice(movieIndex, 1);
-
-  return res.status(204).json({ message: "Movie deleted" });
-});
+router.get("/", MovieController.getAll);
+router.post("/", MovieController.create);
+router.get("/:id", MovieController.getById);
+router.delete("/:id", MovieController.delete);
+router.patch("/:id", MovieController.update);
 
 export default router;
